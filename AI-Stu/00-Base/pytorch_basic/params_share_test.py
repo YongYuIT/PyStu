34c550a819e414aee6bd340e68b530443bcbb2bf
YongYuIT <1198749126@qq.com>
2023-08-29 18:30:06 +0800

    finish params share test

---
+++ AI-Stu/00-Base/pytorch_basic/params_share_test.py
@@ -0,0 +1,15 @@
+# 本示例着重体验torch.nn.Module中参数共享
+
+from torch import nn
+
+# 共享层
+shared = nn.Linear(8, 8)
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(),
+                    shared, nn.ReLU(),
+                    shared, nn.ReLU(),
+                    nn.Linear(8, 1))
+# 检查参数是否相同
+print(net[2].weight.data[0] == net[4].weight.data[0])
+net[2].weight.data[0, 0] = 100
+# 确保它们实际上是同一个对象，而不只是有相同的值
+print(net[2].weight.data[0] == net[4].weight.data[0])
