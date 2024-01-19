af52f44d59b76152a2d19bc750bc367349dcf89b
YongYu_IT <1198749126@qq.com>
2024-01-19 22:19:08 +0800

    初步跑通模型，待验证有效性

--- AI-Stu/03-GenerativeAdversarialNetwork/handwrite_number_gen/test_5_Gen_FC.py
+++ AI-Stu/03-GenerativeAdversarialNetwork/handwrite_number_gen/test_5_Gen_FC.py
@@ -8,7 +8,7 @@ sys.path.append('../../Tasks/Task1')
 from Tools import ShowDict as SD
 
 learningRate = 0.00001 # 模型数据必须从较小设置起，否则容易梯度爆炸出现nan
-numEpochs = 100
+numEpochs = 10000
 
 modelLoad = LN_MD.LeNetGPUModelDef()
 modelLoad.loadModel("LeNetGPUModelDef")
@@ -21,4 +21,8 @@ inputTestX = torch.normal(0, 1, (500, 1000, 100))
 
 dictTrainRecords = model.train_model(inputX, inputTestX)
 
+# 保存模型
+model.saveModel("FCGenModelDef")
+
 SD.showDict("FCGenModelDef", dictTrainRecords, "epoch", "test")
+
